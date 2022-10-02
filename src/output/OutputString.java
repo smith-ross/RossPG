55620939496a21d6d2c731a06860d07c54821919
smith-ross <smithross2003@gmail.com>
2022-10-02 21:18:58 +0100

    Added clean and reusable way of writing to console

---
+++ src/output/OutputString.java
@@ -0,0 +1,23 @@
+package output;
+
+public class OutputString {
+	String lineString;
+	String outputColor = "";
+	
+	public OutputString(String lineString) {
+		this.lineString = lineString;
+	}
+	
+	public OutputString(String lineString, String outputColor) {
+		this.lineString = lineString;
+		this.outputColor = outputColor;
+	}
+	
+	public void write() {
+		System.out.print(this.outputColor + this.lineString + ConsoleColors.RESET);
+	}
+	
+	public void writeLine() {
+		System.out.println(this.outputColor + this.lineString + ConsoleColors.RESET);
+	}
+}
